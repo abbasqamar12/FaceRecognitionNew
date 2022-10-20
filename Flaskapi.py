bbe431523fcadd5e973792f76d4795008939d572
admin <r.q.abbas@gmail.com>
2022-10-20 15:41:06 +0530

    Face2.0 first commit

---
+++ Flaskapi.py
@@ -0,0 +1,19 @@
+from flask import Flask , jsonify , request
+import face_analysis_app
+
+app= Flask(__name__)
+
+@app.route('/' , methods= ['GET' , 'POST'])
+def index():
+    if (request.method == 'GET'):
+        data= face_analysis_app.run()
+        print('face_analysis_app.run')
+        return jsonify({'Age Range':data})
+    elif (request.method == 'POST'):
+        data = face_analysis_app.run()
+        print('face_analysis_app.run')
+        return jsonify({'Age Range':data})
+
+
+if __name__ == "__main__":
+    app.run( port=3000, debug=True)
---
+++ README.md
@@ -0,0 +1,8 @@
+
+  pip install -r requirements.txt
+
+- `face_analysis_app.py` is the main Python file of Streamlit Web-Application. 
+- To run app, write following command in CMD. or use any IDE.
+
+  streamlit run face_analysis_app.py
+
